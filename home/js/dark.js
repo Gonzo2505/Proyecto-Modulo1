4d04e2f40337ea23aa76ddddb45c6c3145ebf0f2
Gonzo2505 <157253155+Gonzo2505@users.noreply.github.com>
2024-04-12 12:52:06 -0500

    Proyecto---Modulo1

---
+++ home/js/dark.js
@@ -0,0 +1,7 @@
+const buttonBlack = document.querySelector("#buttonBlack");
+const elementBody = document.querySelector("body");
+
+buttonBlack.addEventListener("click", function() {
+    document.body.style.backgroundColor = "black";
+});
+
